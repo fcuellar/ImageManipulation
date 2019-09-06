66f95166292e03051bf967525bf19859da4752c4
fcuellar <frankiecuellar@hotmail.com>
2019-09-06 07:26:42 -0400

    importing project

---
+++ ImageManipulation/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace ImageManipulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.fileAddress = new System.Windows.Forms.TextBox();
+            this.newAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.customH = new System.Windows.Forms.TextBox();
+            this.customW = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(638, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(24, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(247, 281);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(532, 243);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(74, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(638, 244);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // fileAddress
+            // 
+            this.fileAddress.Location = new System.Drawing.Point(277, 63);
+            this.fileAddress.Name = "fileAddress";
+            this.fileAddress.ReadOnly = true;
+            this.fileAddress.Size = new System.Drawing.Size(329, 22);
+            this.fileAddress.TabIndex = 5;
+            // 
+            // newAddress
+            // 
+            this.newAddress.Location = new System.Drawing.Point(279, 103);
+            this.newAddress.Name = "newAddress";
+            this.newAddress.ReadOnly = true;
+            this.newAddress.Size = new System.Drawing.Size(327, 22);
+            this.newAddress.TabIndex = 6;
+            this.newAddress.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(429, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "New File Type";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(502, 141);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(104, 24);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.ComboBox2_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(638, 142);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Resize";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // customH
+            // 
+            this.customH.Location = new System.Drawing.Point(331, 186);
+            this.customH.Name = "customH";
+            this.customH.Size = new System.Drawing.Size(66, 22);
+            this.customH.TabIndex = 10;
+            this.customH.Visible = false;
+            // 
+            // customW
+            // 
+            this.customW.Location = new System.Drawing.Point(331, 139);
+            this.customW.Name = "customW";
+            this.customW.Size = new System.Drawing.Size(66, 22);
+            this.customW.TabIndex = 11;
+            this.customW.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(281, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Width";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(276, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Height";
+            this.label3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.customW);
+            this.Controls.Add(this.customH);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.newAddress);
+            this.Controls.Add(this.fileAddress);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Image Manipulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox fileAddress;
+        private System.Windows.Forms.TextBox newAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox customH;
+        private System.Windows.Forms.TextBox customW;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ImageManipulation/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageManipulation
+{
+    public partial class Form1 : Form
+    {
+        Image newImage;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //new open file dialog with filters set for specific types
+            OpenFileDialog openF = new OpenFileDialog();
+            openF.Filter = "Image Files(*.jpg; *.png; *.bmp)|*.jpg;  *.png; *.bmp";
+
+            //if opened successfully 
+            if (openF.ShowDialog()== System.Windows.Forms.DialogResult.OK)
+            {
+                //opens and shows opened image
+                pictureBox1.ImageLocation = openF.FileName;
+                //creates a "copy" image from opened image
+                newImage = Image.FromFile(openF.FileName);
+                //assigned the file address from the opened file
+                fileAddress.Text = openF.FileName;
+                if(button2.Enabled==false || button3.Enabled==false)
+                {
+                    //resets the submit and resize buttons after opening new file
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                }
+                
+            }
+            
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //initializes the dropdown menus with text
+            comboBox1.Items.Add("PNG");
+            comboBox1.Items.Add("JPG");
+            comboBox1.Items.Add("BMP");
+
+            comboBox2.Items.Add("640x480");
+            comboBox2.Items.Add("960x720");
+            comboBox2.Items.Add("1024x768");
+            comboBox2.Items.Add("Other..");
+            
+
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //if no file opened
+            
+            if(fileAddress.Text.Equals(""))
+                {
+                MessageBox.Show("Please pick a File");
+                
+            }
+            
+            else
+            {
+                //removes the last 3 characters from the file address into a new address
+                newAddress.Text = fileAddress.Text.Remove(fileAddress.TextLength - 3);
+                //cases for which type of file was selected
+                
+                if (comboBox1.SelectedItem.Equals("PNG"))
+                {
+                    //image will be saved, adding the format to the end of the address and changing the actual image format
+                    newImage.Save(newAddress.Text + "png", System.Drawing.Imaging.ImageFormat.Png);
+                    //disables button, must open new file
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+
+                    MessageBox.Show("Image has been saved");
+
+                }
+                else if (comboBox1.SelectedItem.Equals("JPG"))
+                {
+                    newImage.Save(newAddress.Text + "jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+
+                    MessageBox.Show("Image has been saved");
+                }
+                else if (comboBox1.SelectedItem.Equals("BMP"))
+                {
+                    newImage.Save(newAddress.Text + "bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+
+                    MessageBox.Show("Image has been saved");
+                }
+                else
+                    MessageBox.Show("Please pick a New File Type");
+            }
+            
+
+
+
+
+        }
+
+        private void ComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //custom resolution will only appear if its clicked on in the drop down menu
+            if(comboBox2.SelectedIndex.Equals(3))
+            {
+                customH.Visible = true;
+                customW.Visible = true;
+                label2.Visible = true;
+                label3.Visible = true;
+
+            }
+            else
+            {
+                customH.Visible = false;
+                customW.Visible = false;
+                label2.Visible = false;
+                label3.Visible = false;
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            //different cases for pre-determined resolutions
+            if (comboBox2.SelectedIndex.Equals(0))
+            {
+                //will call the resize method and disable the button
+                newImage=resize(newImage, 640, 480);
+                button3.Enabled = false;
+                MessageBox.Show("Image has been resized");
+
+            }
+            else if (comboBox2.SelectedIndex.Equals(1))
+            {
+                newImage=resize(newImage, 960, 720);
+                button3.Enabled = false;
+                MessageBox.Show("Image has been resized");
+
+
+            }
+            else if (comboBox2.SelectedIndex.Equals(2))
+            {
+
+                newImage=resize(newImage, 1020, 768);
+                button3.Enabled = false;
+                MessageBox.Show("Image has been resized");
+
+
+
+            }
+            else if (comboBox2.SelectedIndex.Equals(3))
+            {
+                //custom resolution converts the w and the h from the specific text box
+                int w = Convert.ToInt32(customW.Text);
+                int h = Convert.ToInt32(customH.Text);             
+                newImage=resize(newImage, w, h);
+                button3.Enabled = false;
+                MessageBox.Show("Image has been resized");
+
+
+            }
+
+
+
+
+        }
+        //resize method taking image with  new resolution
+        Image resize(Image image, int w, int h)
+        {
+            //new bitmap in pixels with the new resolution size
+            Bitmap rsize = new Bitmap(w, h);
+            //graphical surface to create new image one
+            Graphics graphic = Graphics.FromImage(rsize);
+            //new image created from the origin
+            graphic.DrawImage(image, 0, 0, w, h);
+            graphic.Dispose();
+            return rsize;
+        }
+    }
+}
